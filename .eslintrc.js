03f7fce48516ee91e65d23a956a3e4e06e630c71
id77 <9592236+id77@users.noreply.github.com>
2017-08-11 10:30:06 +0800

    新添动态加载支持

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,61 @@
 module.exports = {
+    "parser": "babel-eslint",
+    // "parserOptions”: {
+    //     "parser": "babel-eslint"
+    // },
     "extends": "airbnb",
     "rules": {
-        'max-len': ["error", 200],
+        'max-len': [
+            "error", 200
+        ],
         // 'no-alert': 'off',
         'no-new': 'off',
-		"no-console": "off", //允许console
-		"indent": ["error", 4], //4个空格缩进TabSize：4
-        'comma-dangle': ["error", "only-multiline"], //不强制尾逗号
-        'array-bracket-spacing': ["error", "never"], // 不用空格
-        'space-in-parens': ["error", "never"], // 不用空格
-        'object-curly-spacing': ["error", "never"], // 不用空格
-		"no-plusplus": ["off", { "allowForLoopAfterthoughts": true }], //支持++,++浏览器做了优化
-		"no-restricted-syntax": [
-			"error",
-			// "ForInStatement",	//允许for in
-			// "ForOfStatement",	//允许for of
-			"LabeledStatement",
-			"WithStatement",
-		],
-		"import/no-extraneous-dependencies": 0, //禁止使用外来包
-		// "import/no-webpack-loader-syntax": 0,
-		// "import/no-unresolved": 0,
-		// "sort-imports": ["error", {   // imports规则(http://eslint.org/docs/rules/sort-imports#rule-details)
-		// 	"ignoreCase": false,
-		// 	"ignoreMemberSort": false,
-		// 	"memberSyntaxSortOrder": ["none", "all", "multiple", "single"]
-		// }]
-	},
-	"env": {
+        // "no-undef": "off",
+        // "no-unused-vars": 'off',
+        "no-console": "off", //允许console
+        "indent": [
+            "error", 4
+        ], //4个空格缩进TabSize：4
+        'comma-dangle': [
+            "error", "only-multiline"
+        ], //不强制尾逗号
+        'array-bracket-spacing': [
+            "error", "never"
+        ], // 不用空格
+        'space-in-parens': [
+            "error", "never"
+        ], // 不用空格
+        'object-curly-spacing': [
+            "error", "never"
+        ], // 不用空格
+        "no-plusplus": [
+            "off", {
+                "allowForLoopAfterthoughts": true
+            }
+        ], //支持++,++浏览器做了优化
+        "no-restricted-syntax": [
+            "error",
+            // "ForInStatement",	//允许for in
+            // "ForOfStatement",	//允许for of
+            "LabeledStatement",
+            "WithStatement"
+        ],
+        "import/no-extraneous-dependencies": 0, //禁止使用外来包
+        // "import/no-webpack-loader-syntax": 0,
+        // "import/no-unresolved": 0,
+        // "sort-imports": ["error", {   // imports规则(http://eslint.org/docs/rules/sort-imports#rule-details)
+        // 	"ignoreCase": false,
+        // 	"ignoreMemberSort": false,
+        // 	"memberSyntaxSortOrder": ["none", "all", "multiple", "single"]
+        // }]
+    },
+    "env": {
         "browser": true,
         "node": true,
         "es6": true,
         "jquery": true,
         // "commonjs": true,
     },
-	"globals":{
-	},
-	"plugins": [
-	]
+    "globals": {},
+    "plugins": []
 };
--- src/js/page/index.js
+++ src/js/page/index.js
@@ -4,13 +4,44 @@ import '../../css/common/global.css';
 import '../../css/common/grid.css';
 import '../../css/page/index.css';
 
+
+function getComponent() {
+    return import(/* webpackChunkName: "lodash" */ 'lodash').then((_) => {
+        const element = document.createElement('div');
+        element.innerHTML = _.join([
+            'Hello', 'webpack'
+        ], ' ');
+        return element;
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+async function getComponent2() {
+    const element = document.createElement('div');
+    const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+
+    element.innerHTML = _.join([
+        'Hello', 'webpack'
+    ], ' ');
+
+    return element;
+}
+
 /* eslint-disable no-undef */
 $('.g-bd').append('<p class="text">这是由js生成的一句话。</p>');
 
-// 增加事件
 $('.btn').click(() => {
     require.ensure(['../components/dialog/index.js'], (require) => {
         const Dialog = require('../components/dialog/index.js');
         new Dialog();
+    });
+
+    getComponent().then((component) => {
+        document.querySelector('.g-hd').appendChild(component);
+    });
+
+    getComponent2().then((component) => {
+        document.querySelector('.g-bd').appendChild(component);
     });
 });
